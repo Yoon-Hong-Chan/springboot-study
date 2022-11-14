94d1641dc3ba81cb184cb3f1095857c0f9da6263
Yoon-Hong-Chan <ychqwertyuio@gmail.com>
2022-11-15 00:42:16 +0900

    feat: spring property 추가

--- src/main/java/me/yoonhc/springbootstudy/etc/HolomanRunner.java
+++ src/main/java/me/yoonhc/springbootstudy/etc/HolomanRunner.java
@@ -2,17 +2,30 @@ package me.yoonhc.springbootstudy.etc;
 
 import me.yoonhc.Holoman;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HolomanRunner implements ApplicationRunner {
+    @Value("${admin.fullName}")
+    private String adminFullName;
+
+    @Value("${admin.name}")
+    private String adminName;
+
+    @Value("${admin.age}")
+    private int age;
+
     @Autowired
     Holoman holoman;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        System.out.println("adminFullName : "+adminFullName);
+        System.out.println("adminName : "+adminName);
+        System.out.println("age : "+age);
         System.out.println(holoman);
     }
 }
---
+++ src/test/java/me/yoonhc/springbootstudy/SpringbootStudyApplicationTest.java
@@ -0,0 +1,25 @@
+package me.yoonhc.springbootstudy;
+
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootStudyApplicationTest {
+    @Autowired
+    Environment environment;
+
+    @Test
+    public void contextLoad(){
+        assertThat(environment.getProperty("admin.name")).isEqualTo("YoonHongChan2");
+    }
+}
