7d36f59799232855391ac32ab6b94790c3fb19b9
Yoon-Hong-Chan <ychqwertyuio@gmail.com>
2022-11-18 23:03:22 +0900

    feat: Logger 설정

--- src/main/java/me/yoonhc/springbootstudy/Listener/ApplicationStartedListener.java
+++ src/main/java/me/yoonhc/springbootstudy/Listener/ApplicationStartedListener.java
@@ -1,16 +1,19 @@
 package me.yoonhc.springbootstudy.Listener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ApplicationStartedListener implements ApplicationListener<ApplicationStartedEvent> {
+    private Logger logger = LoggerFactory.getLogger(ApplicationStartedListener.class);
 
     @Override
     public void onApplicationEvent(ApplicationStartedEvent applicationStartedEvent) {
-        System.out.println("=======================");
-        System.out.println("Application is starting");
-        System.out.println("=======================");
+        logger.info("=======================");
+        logger.info("Application is starting");
+        logger.info("=======================");
     }
 }
