b6dda1e6fb9993a5457aca9674f2840c727e465d
Yoon-Hong-Chan <ychqwertyuio@gmail.com>
2022-11-27 14:04:35 +0900

    feat: Message convert 기능

--- src/main/java/me/yoonhc/springbootstudy/controller/UserController.java
+++ src/main/java/me/yoonhc/springbootstudy/controller/UserController.java
@@ -1,8 +1,11 @@
 package me.yoonhc.springbootstudy.controller;
 
+import me.yoonhc.springbootstudy.domain.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.annotation.RequestScope;
 
 @RestController
 public class UserController {
@@ -10,5 +13,10 @@ public class UserController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @PostMapping("/users/create")
+    public User create(@RequestBody User user){
+        return user;
     }
 }
