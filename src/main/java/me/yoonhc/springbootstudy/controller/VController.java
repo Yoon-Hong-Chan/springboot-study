3f821a47b66c527bbf29fe298e28b2806c30b818
윤홍찬 <ychqwertyuio@gmail.com>
2022-12-07 22:58:10 +0900

    feat: ExceptionHandler 추가

--- src/main/java/me/yoonhc/springbootstudy/controller/VController.java
+++ src/main/java/me/yoonhc/springbootstudy/controller/VController.java
@@ -3,7 +3,9 @@ package me.yoonhc.springbootstudy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class VController {
@@ -15,4 +17,15 @@ public class VController {
         return "hello";
     }
 
+    @GetMapping("/errorTest")
+    public String errorTest(Model model){
+        model.addAttribute("name","yoonhc");
+        model.addAttribute("age","35");
+        return "hello";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public @ResponseBody String sampleError(Exception e){
+        return e.getMessage();
+    }
 }
