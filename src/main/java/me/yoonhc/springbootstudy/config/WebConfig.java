e4437a5d213d3afcc9e2ac624225a7f70724848f
윤홍찬 <ychqwertyuio@gmail.com>
2022-12-10 22:19:24 +0900

    feat: cors 허용 추가

--- src/main/java/me/yoonhc/springbootstudy/config/WebConfig.java
+++ src/main/java/me/yoonhc/springbootstudy/config/WebConfig.java
@@ -1,6 +1,7 @@
 package me.yoonhc.springbootstudy.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
@@ -14,5 +15,11 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/m/**")
                 .addResourceLocations("classpath:/m/")
                 .setCachePeriod(20);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:8081");
     }
 }
--- src/main/java/me/yoonhc/springbootstudy/controller/HelloController.java
+++ src/main/java/me/yoonhc/springbootstudy/controller/HelloController.java
@@ -1,11 +1,13 @@
 package me.yoonhc.springbootstudy.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @CrossOrigin(origins = "http://localhost:8081")
     @RequestMapping("/helloTest")
     public String Hello(){
         return "hello";
